86d95a433da3d01f147e97941d2b975f4bca54d1
dmasdi123 <febriandimas634@gmail.com>
2022-05-21 20:45:19 +0700

    update and delete fiture

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -43,17 +43,17 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        $validateData = $request->validate([
+        $validatedData = $request->validate([
             'title' => 'required|max:255',
             'category_id' => 'required',
             'slug' => 'required|unique:posts',
             'body' => 'required'
         ]);
 
-        $validateData['user_id'] = auth()->user()->id;
-        $validateData['excerpt'] = Str::limit(strip_tags($request->body), 100);
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 100);
 
-        Post::create($validateData);
+        Post::create($validatedData);
         return redirect('/dashboard/posts')->with('success', 'Post Berhasil Ditambahkan');
         // return $request;
     }
@@ -79,7 +79,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -91,7 +94,23 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validatedData = $request->validate($rules);
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 100);
+
+        Post::where('id', $post->id)
+            ->update($validatedData);
+        return redirect('/dashboard/posts')->with('success', 'Post Berhasil Diupdate');
     }
 
     /**
@@ -102,7 +121,8 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+        return redirect('/dashboard/posts')->with('success', 'Post Berhasil Dihapus');
     }
 
     // public  function checkSlug(Request $request) {
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -6,15 +6,17 @@
        <div class="col-lg-8">
           <h1 class="mb-4">{{ $post->title }}</h1>
             <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my Posts</a>
-            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit My Post</a>
-            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete this Post</a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit My Post</a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+               @method('delete')
+               @csrf
+               <button class="btn btn-danger" onclick="return confirm('ingin menghapus data ini ?')"><span data-feather="x-circle"></span> Delete this Post</button>
+             </form>
              <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
  
              <article class="my-3 fs-5">
                 {!! $post->body !!}
              </article>
- 
-          <a href="/blog" class="d-block">Kembali</a>
        </div>
     </div>
  </div>
